e6add84f718e69bdb0fbe2b1c78e821ce62fd5e8
Erin <19705662@students.lincoln.ac.uk>
2020-03-24 15:00:14 +0000

    Moved files here to put on github

---
+++ GitDiff/Commands/Commands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GitDiff
+{
+    class Commands
+    {
+        public void diff(string file1, string file2)
+        {
+            //checks if the contents of both files are equal by reading the lines, then assigns the result as a boolean
+            bool diffEqual = File.ReadLines($"Assets/{file1}").SequenceEqual(File.ReadLines($"Assets/{file2}")); //checks if the contents of both files are equal by reading the lines, then assigns the result as a boolean
+            //outputs the correct statement based on whether or not the files are equal or not
+            switch (diffEqual)
+            {
+                case true:
+                    Console.WriteLine($"{file1} and {file2} are equal");
+                    break;
+                case false:
+                    Console.WriteLine($"{file1} and {file2} are not equal");
+                    break;
+            }
+        }
+    }
+}
---
+++ GitDiff/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace GitDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool filesValid = false;
+            string file;
+            Console.WriteLine("Command: diff file1 file2 (include .txt)");
+            //loops until two existing files are selected
+            while(filesValid == false)
+            {
+                file = Console.ReadLine();
+                //splits the string entered to ensure that the file names can be accessed
+                string[] files = file.Split(" ");
+                //checks if the two files exist so that they can be accessed
+                if (File.Exists($"Assets/{files[1]}") && File.Exists($"Assets/{files[2]}"))
+                {
+                    //checks the difference between the two files
+                    Commands commands = new Commands();
+                    commands.diff(files[1], files[2]);
+                    filesValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Files not found, please try again");
+                }
+
+            }
+        }
+    }
+}
